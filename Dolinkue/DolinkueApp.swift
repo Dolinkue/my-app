96e646e56cc9a26d44df6cf3fa3329ad762fbec7
dolinkue <dolinkue_n@me.com>
2022-03-05 15:27:11 -0300

    Initial Commit

---
+++ Dolinkue/DolinkueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DolinkueApp.swift
+//  Dolinkue
+//
+//  Created by Nicolas Dolinkue on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DolinkueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
